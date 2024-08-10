d73f3f3c75fe63e320189de2bdd5efb76e499f7e
beetrbgus <devbeetrb0214@gmail.com>
2024-08-11 00:45:36 +0900

    feat - supabase Storage에서 file 가져오기

---
+++ lib/my_image.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class MyImage extends StatefulWidget {
+  const MyImage({super.key});
+
+  @override
+  State<MyImage> createState() => _MyImageState();
+}
+
+class _MyImageState extends State<MyImage> {
+  final _future = Supabase.instance.client.storage
+      .from(dotenv.get("BUCKET_NAME"))
+      .download('public/cat.jpeg');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final file = snapshot.data!;
+
+          return Image.memory(file);
+        },
+      ),
+    );
+  }
+}
